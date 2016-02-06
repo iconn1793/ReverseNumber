97ceedaef3e99fcafc0adfad227c5e1d58ae20c0
iconn1793 <ianconnelly@Ians-MacBook-Pro.local>
2016-02-05 16:56:22 -0800

    initial proj

---
+++ src/ReverseNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		System.out.print("Please provide an integer: ");
+		int input = kb.nextInt(); 
+				
+		while (input == 0) {
+			System.out.print("Please select a non-zero integer: ");
+			input = kb.nextInt();
+		}
+		
+		String inputString = Integer.toString(input);
+		int n = inputString.length();
+				
+		for (int i = 0; i < n; i++){
+			
+			int tmp = input%10; 
+			input = (input - tmp)/10;
+			System.out.print(tmp);
+			
+		}
+		
+		System.out.println();
+		kb.close();
+	}
+
+}
