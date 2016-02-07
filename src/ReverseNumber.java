97b1021ef88c3af985947d3fb049f45e0278c5d4
iconn1793 <ianconnelly@Ians-MacBook-Pro.local>
2016-02-07 12:28:51 -0800

    Created a method that returns flipped in, not just prints

--- src/ReverseNumber.java
+++ src/ReverseNumber.java
@@ -2,36 +2,61 @@ import java.util.Scanner;
 
 
 public class ReverseNumber {
-
+	
+	static Scanner kb = new Scanner(System.in);
+	static int input;
+	
+	
 	public static void main(String[] args) {
-		
-		Scanner kb = new Scanner(System.in);
 		System.out.print("Please provide an integer: ");
-		int input = kb.nextInt(); 
-				
-		while (input == 0) {
+		int userNum = kb.nextInt(); 
+						
+		while (userNum == 0) {
 			System.out.print("Please select a non-zero integer: ");
 			input = kb.nextInt();
 		}
 		
+		int flipped = flipInt(userNum);
+		System.out.println(flipped);
+		
+	}	
+		
+	public static int flipInt(int input){
+		int newInt = 0;
+		boolean neg = false;
+		
 		if (input < 0){
-			input = Math.abs(input);
-			System.out.print("-");
+			neg = true;
 		}
+		
+		input = Math.abs(input);
 		
 		String inputString = Integer.toString(input);
 		int n = inputString.length();
+		int[] reversedNum = new int[n];
 		
+		//creates reversed digit array
 		for (int i = 0; i < n; i++){
 			
 			int tmp = input%10; 
 			input = (input - tmp)/10;
-			System.out.print(tmp);
+			reversedNum[i] = tmp;
 			
 		}
 		
-		System.out.println();
+		//creates new/composited int
+		for (int k = 0; k < n; k++){
+			//
+			int tmp = (int) (Math.pow(10,((n-1)-k)) * reversedNum[k]);
+			newInt = newInt + tmp;
+		}
+		
+		if (neg){
+			newInt = newInt - (newInt*2);
+		}
+		
 		kb.close();
+		return newInt; 
 	}
 
 }
