480ad1b068fe7e04b4dd7966a2bd35dab40b89a5
iconn1793 <ianconnelly@Ians-MacBook-Pro.local>
2016-02-09 11:10:49 -0800

    removed use of string conversion

--- src/ReverseNumber.java
+++ src/ReverseNumber.java
@@ -22,39 +22,26 @@ public class ReverseNumber {
 	}	
 		
 	public static int flipInt(int input){
-		int newInt = 0;
 		boolean neg = false;
+		int result = 0;
 		
 		if (input < 0){
 			neg = true;
+			input = Math.abs(input);
 		}
-		
-		input = Math.abs(input);
-		
-		String inputString = Integer.toString(input);
-		int n = inputString.length();
-		int[] reversedNum = new int[n];
-		
-		//creates reversed digit array
-		for (int i = 0; i < n; i++){
+	
+		while (input != 0){
 			
-			int tmp = input%10; 
-			input = (input - tmp)/10;
-			reversedNum[i] = tmp;
+			result = (result*10) + (input%10);
+			input = input/10; 
 			
 		}
-		
-		//creates new/composited int
-		for (int k = 0; k < n; k++){
-			 newInt = newInt + (int) (Math.pow(10,((n-1)-k)) * reversedNum[k]);
-		}
-		
+				
 		if (neg){
-			newInt = 0 - newInt; 
+			result = 0 - result; 
 		}
 		
 		kb.close();
-		return newInt; 
+		return result; 
 	}
-
 }
